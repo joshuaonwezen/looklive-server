8a22080f730008109b2ac92a0d3b01e537dbffa6
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-03-17 20:22:58 +0100

    service worker fixes

--- public/js/app.js
+++ public/js/app.js
@@ -24,18 +24,21 @@
         }
 
     }
+    
+    function onLoad() {
+        route();
+        renderAppereance();
+    }
 
-    function appearance() {
-        var firstProduct = document.querySelector('.product');
-        var firstIndicator = document.querySelector(
-                '.product-indicator[data-uuid="' + firstProduct.getAttribute('data-uuid') + '"]'
-                );
-        var indicators = document.querySelectorAll('.product-indicator');
+    function renderAppereance() {
+        var product = document.querySelector('.product');
+        var indicator = document.querySelector('.product-indicator[data-uuid="' + product.getAttribute('data-uuid') + '"]');
+        var allIndicators = document.querySelectorAll('.product-indicator');
 
-        firstProduct.classList.add('product-active');
-        firstIndicator.classList.add('product-indicator-active');
+        product.classList.add('product-active');
+        indicator.classList.add('product-indicator-active');
 
-        Array.prototype.forEach.call(indicators, function (btn) {
+        Array.prototype.forEach.call(allIndicators, function (btn) {
             btn.addEventListener('click', function (event) {
                 var id = event.currentTarget.getAttribute('data-uuid');
                 document.querySelector('.product-active').classList.remove('product-active');
@@ -46,83 +49,34 @@
         });
     }
 
-    
-        function spa() {
-        var anchors = document.querySelectorAll('a[href^="/"]');
 
-        Array.prototype.forEach.call(anchors, function (anchor) {
-            anchor.addEventListener('click', function (event) {
+    function route() {
+        var allRoutes = document.querySelectorAll('a[href^="/"]');
+
+        Array.prototype.forEach.call(allRoutes, function (route) {
+            route.addEventListener('click', function (event) {
                 event.preventDefault();
-
                 var href = event.currentTarget.getAttribute('href');
                 history.pushState(null, null, href);
                 fetchHTML(href);
             });
         });
 
-        window.addEventListener('popstate', function() {
+        window.addEventListener('popstate', function () {
             fetchHTML(window.location.pathname);
         });
     }
 
-    /**
-     * Fetch the HTML for the href that is passed in.
-     *
-     * @param  {String} href
-     * @return {void}
-     */
+
     function fetchHTML(href) {
-       fetch('http://0.0.0.0:3000/api' + href)
-           .then(function(response) { return response.text(); })
-           .then(function(body) {
-               document.querySelector('.inner-wrapper')
-                   .innerHTML = body;
-
-               onPageLoad();
-           });
+        fetch('http://localhost:3000/api' + href).then(function (response) {
+            return response.text();
+        }).then(function (body) {
+            document.querySelector('.inner-wrapper').innerHTML = body;
+            onLoad();
+        });
     }
 
-    /**
-     * Logic that should be invoked on every page load. Also from changing URL
-     * with pushstate/popstate.
-     *
-     * @return {void}
-     */
-    function onPageLoad() {
-        spa();
-        appearance();
-
-        
-        if ('serviceWorker' in navigator) {
-
-            document.getElementById('register').onclick = function () {
-                navigator.serviceWorker.register('sw.js', {scope: './'})
-                        .then(function (reg) {
-                            app.output('registered sw (see console)');
-                            console.info('registered sw', reg);
-                        })
-                        .catch(function (err) {
-                            console.error('error registering sw', err);
-                        });
-            };
-
-            document.getElementById('unregister').onclick = function () {
-                navigator.serviceWorker.getRegistration('./')
-                        .then(function (reg) {
-                            reg.unregister();
-                            app.output('unregistered sw');
-                        })
-                        .catch(function (err) {
-                            app.output('error unregistering sw (see console)');
-                            console.error('error unregistering sw', err);
-                        });
-            };
-
-        } else {
-            app.output('ServiceWorker is not supported');
-        }
-    }
-
-    ready(onPageLoad);
+    ready(onLoad);
 
 }());
